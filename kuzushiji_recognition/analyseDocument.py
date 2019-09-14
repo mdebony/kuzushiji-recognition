ae7a2c9e92d4ddb586d5978a4b4af0f08a0cd218
Mathieu <mathieu-de-bony@orange.fr>
2019-09-14 11:20:25 +0200

    Start implementation of the main function for analyse a document

--- kuzushiji_recognition/analyseDocument.py
+++ kuzushiji_recognition/analyseDocument.py
@@ -1,10 +1,52 @@
+from tensorflow import keras
 import scipy
 
 
 def analyseDocument(importFile, outputFile, folderImage, unicodeFile, modelCaracterEmplacement, modelCaracterRecognition,
-                    caracterEmplacementRes = (1024, 1024), caracacterEmplacementIsGrey=True,
-                    caracterRecognitionRes = (32, 32), caracterRecognitionIsGrey = True):
+                    caracterEmplacementRes = (1024, 1024), caracacterEmplacementIsGrey=True, caracterEmplacementBatchSize = 16,
+                    caracterRecognitionRes = (32, 32), caracterRecognitionIsGrey = True, caracterRecognitionBatchSize = 256,
+                    thresholdEmplacement = 0.5):
     
+    print("Load model caracter emplacement")
+    model = keras.models.load_model(modelCaracterEmplacement)
+    
+    print("Load dataset caracter emplacement")
+    imageDataset, xInit, yInit, xThumb, yThumb, xShift, yShift = loadImageCaracterEmplacement(importFile,
+                                                                                              folderImage, caracterEmplacementRes = caracterEmplacementRes,
+                                                                                              caracacterEmplacementIsGrey=caracacterEmplacementIsGrey)
+    imageDataset = (imageDataset/255).astype(np.float16)
+    
+    print("Predict caracter emplacement")
+    segmentationMaps = model.predict(imageDataset = imageDataset, batch_size=caracterEmplacementBatchSize)
+    del imageDataset
+    del model
+    
+    print("Create emplacement database")
+    database = createEmplacementDatabase(segmentationMaps, thresholdEmplacement)
+    del segmentationMaps
+    
+    
+    print("Load model caracter recognition")
+    model = keras.models.load_model(modelCaracterRecognition)
+    
+    print("Load caracter dataset")
+    imageDataset = loadImageCaracterRecognition(database, folderImage,
+                                                caracterRecognitionRes = modelCaracterRecognition,
+                                                caracterRecognitionIsGrey = caracterRecognitionIsGrey):
+    imageDataset = (imageDataset/255).astype(np.float16)
+    
+    print("Predict caracter")
+    caracterValue = model.predict(imageDataset = imageDataset, batch_size=caracterEmplacementBatchSize)
+    del imageDataset
+    del model
+    
+    
+    print("Create output file")
+    createFinalFile(database, outputFile, unicodeFile)
+    
+    
+def createEmplacementDatabase(segmentationMaps, threshold = 0.5)
+
     pass
 
 
