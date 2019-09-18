ad1431504c645448c989f8839a8aa64a1c07cc0c
adelpierre <adri.delpierre@hotmail.fr>
2019-09-18 22:28:30 +0200

    Various changes

--- kuzushiji_recognition/modelsGenerator.py
+++ kuzushiji_recognition/modelsGenerator.py
@@ -18,6 +18,7 @@ def trainAndEvaluateModel(model, filenameModel, batchSize, inputData, outputData
     callbacks = [checkpoint, history, reduceLR, earlyStop]
 
     model.compile(optimizer='adam', loss=lossFunc, metrics=['accuracy'])
+#    model.summary() #DeleteLater
     model.fit(inputData[:nVal], outputData[:nVal], epochs=maxEpochs, batch_size=batchSize,
               validation_data=(inputData[nVal:nTest], outputData[nVal:nTest]), shuffle=True,
               callbacks=callbacks, verbose=1)
@@ -46,78 +47,82 @@ def createKMNISTModel1(inputShape, outputLength, dropoutRate, convLayer, denseLa
     
     return model
 
-def createModelUNet(inputs, dropoutRate, convLayer, denseLayer):
-    inputs = Input(inputs.shape, name='input')
+def createModelUNet(inputShape, dropoutRate, convLayer):
+    inputLayer = keras.Input(inputShape, name='input')
     #1st Stage
-    conv1a = Conv2D(convLayer, (3, 3), activation='relu', name='conv1a')(inputs)
-    drop1a = SpatialDropout2D(dropoutRate, name='drop1a')(conv1a)
-    conv1b = Conv2D(convLayer, (3, 3), activation='relu', padding='same', name='conv1b')(drop1a)
-    drop1b = SpatialDropout2D(dropoutRate, name='drop1b')(conv1b)
-    pool1 = MaxPooling2D(pool_size=(2, 2), name='pool1')(drop1b)
+    conv1a = keras.layers.Conv2D(convLayer, (3, 3), activation='relu', padding = 'valid', name='conv1a')(inputLayer)
+    drop1a = keras.layers.SpatialDropout2D(dropoutRate, name='drop1a')(conv1a)
+    conv1b = keras.layers.Conv2D(convLayer, (3, 3), activation='relu', padding='valid', name='conv1b')(drop1a)
+    drop1b = keras.layers.SpatialDropout2D(dropoutRate, name='drop1b')(conv1b)
+    pool1 = keras.layers.MaxPooling2D(pool_size=(2, 2), name='pool1')(drop1b)
     
     #2nd Stage
-    conv2a = Conv2D(convLayer*2, (3, 3), activation='relu', name='conv2a')(pool1)
-    drop2a = SpatialDropout2D(dropoutRate, name='drop2a')(conv2a)
-    conv2b = Conv2D(convLayer*2, (3, 3), activation='relu', name='conv2b')(drop2a)
-    drop2b = SpatialDropout2D(dropoutRate, name='drop2b')(conv2b)
-    pool2 = MaxPooling2D(pool_size=(2, 2), name='pool2')(drop2b)
+    conv2a = keras.layers.Conv2D(convLayer*2, (3, 3), activation='relu', padding = 'valid', name='conv2a')(pool1)
+    drop2a = keras.layers.SpatialDropout2D(dropoutRate, name='drop2a')(conv2a)
+    conv2b = keras.layers.Conv2D(convLayer*2, (3, 3), activation='relu', padding = 'valid', name='conv2b')(drop2a)
+    drop2b = keras.layers.SpatialDropout2D(dropoutRate, name='drop2b')(conv2b)
+    pool2 = keras.layers.MaxPooling2D(pool_size=(2, 2), name='pool2')(drop2b)
     
     #3rd Stage
-    conv3a = Conv2D(convLayer*4, (3, 3), activation='relu', name='conv3a')(pool2)
-    drop3a = SpatialDropout2D(dropoutRate, name='drop3a')(conv3a)
-    conv3b = Conv2D(convLayer*4, (3, 3), activation='relu', name='conv3b')(drop3a)
-    drop3b = SpatialDropout2D(dropoutRate, name='drop3b')(conv3b)
-    pool3 = MaxPooling2D(pool_size=(2, 2), name='pool3')(drop3b)
+    conv3a = keras.layers.Conv2D(convLayer*4, (3, 3), activation='relu', padding = 'valid', name='conv3a')(pool2)
+    drop3a = keras.layers.SpatialDropout2D(dropoutRate, name='drop3a')(conv3a)
+    conv3b = keras.layers.Conv2D(convLayer*4, (3, 3), activation='relu', padding = 'valid', name='conv3b')(drop3a)
+    drop3b = keras.layers.SpatialDropout2D(dropoutRate, name='drop3b')(conv3b)
+    pool3 = keras.layers.MaxPooling2D(pool_size=(2, 2), name='pool3')(drop3b)
     
     #4th Stage
-    conv4a = Conv2D(convLayer*8, (3, 3), activation='relu', name='conv4a')(pool3)
-    drop4a = SpatialDropout2D(dropoutRate, name='drop4a')(conv4a)
-    conv4b = Conv2D(convLayer*8, (3, 3), activation='relu', name='conv4b')(drop4a)
-    drop4b = SpatialDropout2D(dropoutRate, name='drop4b')(conv4b)
-    pool4 = MaxPooling2D(pool_size=(2, 2), name='pool4')(drop4b)
+    conv4a = keras.layers.Conv2D(convLayer*8, (3, 3), activation='relu', padding = 'valid', name='conv4a')(pool3)
+    drop4a = keras.layers.SpatialDropout2D(dropoutRate, name='drop4a')(conv4a)
+    conv4b = keras.layers.Conv2D(convLayer*8, (3, 3), activation='relu', padding = 'valid', name='conv4b')(drop4a)
+    drop4b = keras.layers.SpatialDropout2D(dropoutRate, name='drop4b')(conv4b)
+    pool4 = keras.layers.MaxPooling2D(pool_size=(2, 2), name='pool4')(drop4b)
     
     #5th Stage
-    conv5a = Conv2D(convLayer*16, (3, 3), activation='relu', name='conv5a')(pool4)
-    drop5a = SpatialDropout2D(dropoutRate, name='drop5a')(conv5a)
-    conv5b = Conv2D(convLayer*16, (3, 3), activation='relu', name='conv5b')(drop5a)
-    drop5b = SpatialDropout2D(dropoutRate, name='drop5b')(conv5b)
+    conv5a = keras.layers.Conv2D(convLayer*16, (3, 3), activation='relu', padding = 'valid', name='conv5a')(pool4)
+    drop5a = keras.layers.SpatialDropout2D(dropoutRate, name='drop5a')(conv5a)
+    conv5b = keras.layers.Conv2D(convLayer*16, (3, 3), activation='relu', padding = 'valid', name='conv5b')(drop5a)
+    drop5b = keras.layers.SpatialDropout2D(dropoutRate, name='drop5b')(conv5b)
     
     #6th Stage
-    up6 = Conv2DTranspose(convLayerr*8, (3, 3), activation='relu', name='conv6a')(drop5b)
-    merge6 = concatenate([drop4b, up6], axis=3, name='merge6')
-    conv6b = Conv2D(convLayer*8, (3, 3), activation='relu', name='conv6b')(merge6)
-    drop6b = SpatialDropout2D(dropoutRate, name='drop6b')(conv6b)
-    conv6c = Conv2D(convLayer*8, (3, 3), activation='relu', name='conv6c')(drop6b)
-    drop6c = SpatialDropout2D(dropoutRate, name='drop6c')(conv6c)
+    up6 = keras.layers.Conv2DTranspose(convLayer*8, (3, 3), activation='relu', padding = 'valid', name='conv6a')(drop5b)
+    
+    model = keras.models.Model(inputLayer, drop5b)
+    model.summary()
+    
+    merge6 = keras.layers.concatenate([drop4b, up6], axis=3, name='merge6')
+    conv6b = keras.layers.Conv2D(convLayer*8, (3, 3), activation='relu', padding = 'valid', name='conv6b')(merge6)
+    drop6b = keras.layers.SpatialDropout2D(dropoutRate, name='drop6b')(conv6b)
+    conv6c = keras.layers.Conv2D(convLayer*8, (3, 3), activation='relu', padding = 'valid', name='conv6c')(drop6b)
+    drop6c = keras.layers.SpatialDropout2D(dropoutRate, name='drop6c')(conv6c)
     
     #7th Stage
-    up7 = Conv2DTranspose(convLayer*4, (3, 3), activation='relu', name='conv7a')(drop6c)
-    merge7 = concatenate([drop3b, up7], axis=3, name='merge7')
-    conv7b = Conv2D(convLayer*4, (3, 3), activation='relu', name='conv7b')(merge7)
-    drop7b = SpatialDropout2D(dropoutRate, name='drop7b')(conv7b)
-    conv7c = Conv2D(convLayer*4, (3, 3), activation='relu', name='conv7c')(drop7b)
-    drop7c = SpatialDropout2D(dropoutRate, name='drop7c')(conv7c)
+    up7 = keras.layers.Conv2DTranspose(convLayer*4, (3, 3), activation='relu', padding = 'valid', name='conv7a')(drop6c)
+    merge7 = keras.layers.concatenate([drop3b, up7], axis=3, name='merge7')
+    conv7b = keras.layers.Conv2D(convLayer*4, (3, 3), activation='relu', padding = 'valid', name='conv7b')(merge7)
+    drop7b = keras.layers.SpatialDropout2D(dropoutRate, name='drop7b')(conv7b)
+    conv7c = keras.layers.Conv2D(convLayer*4, (3, 3), activation='relu', padding = 'valid', name='conv7c')(drop7b)
+    drop7c = keras.layers.SpatialDropout2D(dropoutRate, name='drop7c')(conv7c)
     
     #8th Stage
-    up8 = Conv2DTranspose(convLayerr*2, (3, 3), activation='relu', name='conv8a')(drop7c)
-    merge8 = concatenate([drop2b, up8], axis=3, name='merge8')
-    conv8b = Conv2D(convLayer*2, (3, 3), activation='relu', name='conv8b')(merge8)
-    drop8b = SpatialDropout2D(dropoutRate, name='drop8b')(conv8b)
-    conv8c = Conv2D(convLayer*2, (3, 3), activation='relu', name='conv8c')(drop8b)
-    drop8c = SpatialDropout2D(dropoutRate, name='drop8c')(conv8c)
+    up8 = keras.layers.Conv2DTranspose(convLayerr*2, (3, 3), activation='relu', name='conv8a')(drop7c)
+    merge8 = keras.layers.concatenate([drop2b, up8], axis=3, name='merge8')
+    conv8b = keras.layers.Conv2D(convLayer*2, (3, 3), activation='relu', padding = 'valid', name='conv8b')(merge8)
+    drop8b = keras.layers.SpatialDropout2D(dropoutRate, name='drop8b')(conv8b)
+    conv8c = keras.layers.Conv2D(convLayer*2, (3, 3), activation='relu', padding = 'valid', name='conv8c')(drop8b)
+    drop8c = keras.layers.SpatialDropout2D(dropoutRate, name='drop8c')(conv8c)
     
     #9th Stage
-    up9 = Conv2DTranspose(convLayer, (3, 3), activation='relu', name='conv9a')(drop8c)
-    merge9 = concatenate([drop1b, up9], axis=3, name='merge9')
-    conv9b = Conv2D(convLayer, (3, 3), activation='relu', name='conv9b')(merge9)
-    drop9b = SpatialDropout2D(dropoutRate, name='drop9b')(conv9b)
-    conv9c = Conv2D(convLayer, (3, 3), activation='relu', name='conv9c')(drop9b)
-    drop9c = SpatialDropout2D(dropoutRate, name='drop9c')(conv9c)
+    up9 = keras.layers.Conv2DTranspose(convLayer, (3, 3), activation='relu', padding = 'valid', name='conv9a')(drop8c)
+    merge9 = keras.layers.concatenate([drop1b, up9], axis=3, name='merge9')
+    conv9b = keras.layers.Conv2D(convLayer, (3, 3), activation='relu', padding = 'valid', name='conv9b')(merge9)
+    drop9b = keras.layers.SpatialDropout2D(dropoutRate, name='drop9b')(conv9b)
+    conv9c = keras.layers.Conv2D(convLayer, (3, 3), activation='relu', padding = 'valid', name='conv9c')(drop9b)
+    drop9c = keras.layers.SpatialDropout2D(dropoutRate, name='drop9c')(conv9c)
     
     #10th Stage
-    conv10 = Conv2D(convLayer, (3, 3), activation='relu', name='conv10')(drop9c)
+    conv10 = keras.layers.Conv2D(convLayer, (3, 3), activation='relu', padding = 'valid', name='conv10')(drop9c)
     
-    Model(input=inputs, output=conv10)
+    model = keras.models.Model(inputs=inputLayer, output=conv10)
     
     model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
     model.summary()
